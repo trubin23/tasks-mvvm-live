9fb6cb5c274a5bfcd77be77fecc4eddc8ff4893a
Trubin Andrey <trubin23@gmail.com>
2018-12-11 05:50:24 +0500

    add styles for project

--- app/src/main/java/ru/trubin23/tasksmvvmlive/tasks/TasksActivity.java
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/tasks/TasksActivity.java
@@ -1,13 +1,15 @@
-package ru.trubin23.tasksmvvmlive;
+package ru.trubin23.tasksmvvmlive.tasks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ru.trubin23.tasksmvvmlive.R;
+
+public class TasksActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tasks_act);
     }
 }
