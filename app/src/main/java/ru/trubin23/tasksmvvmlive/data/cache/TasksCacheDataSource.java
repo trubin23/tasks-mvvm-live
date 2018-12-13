96a3665d99b9bbe002266a63bffac1a56a1d9f23
Trubin Andrey <trubin23@gmail.com>
2018-12-13 21:52:11 +0500

    add TasksCacheDataSource interface

---
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/data/cache/TasksCacheDataSource.java
@@ -0,0 +1,29 @@
+package ru.trubin23.tasksmvvmlive.data.cache;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.List;
+
+import ru.trubin23.tasksmvvmlive.data.Task;
+
+public interface TasksCacheDataSource {
+
+    void setTasks(@NonNull List<Task> tasks);
+
+    void irrelevantState();
+
+    @Nullable
+    List<Task> getTasks();
+
+    @Nullable
+    Task getTaskById(@NonNull String taskId);
+
+    void addTask(@NonNull Task task);
+
+    void removeTask(@NonNull String taskId);
+
+    void completedTask(@NonNull String taskId, boolean completed);
+
+    void clearCompletedTask();
+}
