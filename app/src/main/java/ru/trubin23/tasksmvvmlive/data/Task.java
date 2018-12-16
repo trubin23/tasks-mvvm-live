a4c9b682cf56368516091391594320d4bbf813fe
Trubin Andrey <trubin23@gmail.com>
2018-12-16 09:50:30 +0500

    implement Task class

--- app/src/main/java/ru/trubin23/tasksmvvmlive/data/Task.java
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/data/Task.java
@@ -1,35 +1,72 @@
 package ru.trubin23.tasksmvvmlive.data;
 
-public class Task {
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
-    private final String mId;
+import com.google.common.base.Strings;
 
+import java.util.UUID;
+
+@Entity(tableName = "tasks")
+public final class Task {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "task_id")
+    private final String mTaskId;
+
+    @NonNull
+    @ColumnInfo(name = "title")
     private final String mTitle;
 
+    @NonNull
+    @ColumnInfo(name = "description")
     private final String mDescription;
 
+    @ColumnInfo(name = "completed")
     private final boolean mCompleted;
 
-    public Task(String id, String title, String description, boolean completed) {
-        mId = id;
+    public Task(@NonNull String title, @NonNull String description,
+                @NonNull String taskId, boolean completed) {
         mTitle = title;
         mDescription = description;
+        mTaskId = taskId;
         mCompleted = completed;
     }
 
+    @Ignore
+    public Task(@NonNull String title, @NonNull String description, @NonNull String taskId) {
+        this(title, description, taskId, false);
+    }
+
+    @Ignore
+    public Task(@NonNull String title, @NonNull String description) {
+        this(title, description, UUID.randomUUID().toString(), false);
+    }
+
+    @NonNull
     public String getTaskId() {
-        return null;
+        return mTaskId;
     }
 
+    @NonNull
     public String getTitle() {
-        return null;
+        return mTitle;
     }
 
+    @NonNull
     public String getDescription() {
-        return null;
+        return mDescription;
     }
 
     public boolean isCompleted() {
-        return false;
+        return mCompleted;
+    }
+
+    public boolean isEmpty() {
+        return Strings.isNullOrEmpty(mTitle) && Strings.isNullOrEmpty(mDescription);
     }
 }
