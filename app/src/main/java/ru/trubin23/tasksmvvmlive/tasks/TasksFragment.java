2dda9fc3405bcf026b26d6d40b82b8d96457b7f4
Trubin Andrey <trubin23@gmail.com>
2018-12-20 22:32:05 +0500

    continue implementation tasks package

---
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/tasks/TasksFragment.java
@@ -0,0 +1,6 @@
+package ru.trubin23.tasksmvvmlive.tasks;
+
+import android.support.v4.app.Fragment;
+
+public class TasksFragment extends Fragment {
+}
---
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/tasks/TasksViewModel.java
@@ -0,0 +1,12 @@
+package ru.trubin23.tasksmvvmlive.tasks;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class TasksViewModel extends AndroidViewModel {
+
+    public TasksViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
