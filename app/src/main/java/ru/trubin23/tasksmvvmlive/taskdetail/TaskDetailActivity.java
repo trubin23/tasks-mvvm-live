ff5602d67a07ff1258f4a6b40bc2683f2f60c923
Trubin Andrey <trubin23@gmail.com>
2018-12-23 11:34:51 +0500

    start to fill tasks_act layout

--- app/src/main/java/ru/trubin23/tasksmvvmlive/taskdetail/TaskDetailActivity.java
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/taskdetail/TaskDetailActivity.java
@@ -3,6 +3,8 @@ package ru.trubin23.tasksmvvmlive.taskdetail;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ru.trubin23.tasksmvvmlive.R;
+
 public class TaskDetailActivity extends AppCompatActivity {
 
     @Override
