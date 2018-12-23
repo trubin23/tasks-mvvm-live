6507f7914e71211b6589edd4cde14502c420a2ce
trubin23 <trubin23@gmail.com>
2018-12-23 18:08:21 +0500

    added activities

---
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/addedittask/AddEditTaskActivity.java
@@ -0,0 +1,15 @@
+package ru.trubin23.tasksmvvmlive.addedittask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.trubin23.tasksmvvmlive.R;
+
+public class AddEditTaskActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_edit_task);
+  }
+}
