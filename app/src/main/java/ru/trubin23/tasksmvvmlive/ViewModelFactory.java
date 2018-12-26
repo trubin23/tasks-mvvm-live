09f47d571b7b8d157cffd88b0c00b7b3e7fa9e0c
Trubin Andrey <trubin23@gmail.com>
2018-12-27 02:38:55 +0500

    add "create" function in ViewModelFactory class

--- app/src/main/java/ru/trubin23/tasksmvvmlive/ViewModelFactory.java
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/ViewModelFactory.java
@@ -1,8 +1,11 @@
 package ru.trubin23.tasksmvvmlive;
 
 import android.app.Application;
+import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import ru.trubin23.tasksmvvmlive.data.source.TasksRepository;
+import ru.trubin23.tasksmvvmlive.tasks.TasksActivity;
+import ru.trubin23.tasksmvvmlive.tasks.TasksViewModel;
 
 public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory {
 
@@ -27,5 +30,13 @@ public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory {
     private ViewModelFactory(Application application, TasksRepository repository){
         mApplication = application;
         mTasksRepository = repository;
+    }
+
+    @Override
+    public <T extends ViewModel> T create(Class<T> modelClass){
+        if (modelClass.isAssignableFrom(TasksActivity.class)){
+            return (T) new TasksViewModel(mApplication, mTasksRepository);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
     }
 }
--- app/src/main/java/ru/trubin23/tasksmvvmlive/tasks/TasksViewModel.java
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/tasks/TasksViewModel.java
@@ -2,12 +2,22 @@ package ru.trubin23.tasksmvvmlive.tasks;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.content.Context;
 import android.support.annotation.NonNull;
+
+import ru.trubin23.tasksmvvmlive.data.source.TasksRepository;
 
 public class TasksViewModel extends AndroidViewModel {
 
-    public TasksViewModel(@NonNull Application application) {
-        super(application);
+    private final Context mContext;
+
+    private final TasksRepository mTasksRepository;
+
+    public TasksViewModel(@NonNull Application context,
+                          @NonNull TasksRepository repository) {
+        super(context);
+        mContext = context.getApplicationContext();
+        mTasksRepository = repository;
     }
 
     void handleActivityResult(int requestCode, int resultCode) {
