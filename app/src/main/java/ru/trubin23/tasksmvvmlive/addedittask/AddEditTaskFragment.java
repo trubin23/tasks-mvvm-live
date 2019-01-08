25961368f67697d5287679fd84301256c951ac28
Trubin Andrey <trubin23@gmail.com>
2019-01-08 18:59:34 +0500

    continue implementation AddEditTaskFragment

--- app/src/main/java/ru/trubin23/tasksmvvmlive/addedittask/AddEditTaskFragment.java
+++ app/src/main/java/ru/trubin23/tasksmvvmlive/addedittask/AddEditTaskFragment.java
@@ -3,12 +3,18 @@ package ru.trubin23.tasksmvvmlive.addedittask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import ru.trubin23.tasksmvvmlive.R;
+import ru.trubin23.tasksmvvmlive.SnackbarMessage;
 import ru.trubin23.tasksmvvmlive.databinding.AddtaskFragBinding;
+import ru.trubin23.tasksmvvmlive.util.SnackbarUtils;
 
 public class AddEditTaskFragment extends Fragment {
 
@@ -31,6 +37,49 @@ public class AddEditTaskFragment extends Fragment {
 
         addtaskFragBinding.setViewmodel(mViewModel);
 
+        setHasOptionsMenu(true);
+
         return addtaskFragBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setupFab();
+
+        setupSnackbar();
+
+        setupActionBar();
+
+        loadData();
+    }
+
+    private void setupFab() {
+        FloatingActionButton fab = getActivity().findViewById(R.id.fab_edit_task_done);
+        fab.setOnClickListener(v -> mViewModel.saveTask());
+    }
+
+    private void setupSnackbar() {
+        mViewModel.getSnackbarMessage().observe(this,
+                (SnackbarMessage.ShackbarObserver) snackbarMessageResId ->
+                        SnackbarUtils.showSnackbar(getView(), getString(snackbarMessageResId))
+        );
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar == null){
+            return;
+        }
+        if (getArguments()!=null && getArguments().get(ARGUMENT_EDIT_TASK_ID)!=null){
+            actionBar.setTitle(R.string.title_edit_task);
+        } else {
+            actionBar.setTitle(R.string.title_add_task);
+        }
+    }
+
+    private void loadData() {
+
     }
 }
